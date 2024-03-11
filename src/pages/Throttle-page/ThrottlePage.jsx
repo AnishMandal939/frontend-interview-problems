2bd3d77cce624ff0c1df3c636ed07cc947ac9a47
AnishMandal939 <mandalktaanees12@gmail.com>
2024-03-12 02:32:09 +0545

    Add throttle hook to handle window resize event

--- src/pages/Throttle-page/ThrottlePage.jsx
+++ src/pages/Throttle-page/ThrottlePage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import useThrottle from "../../custom/hooks/Throttle";
 
 export default function ThrottlePage() {
   const [windowSize, setWindowSize] = useState({
@@ -13,10 +14,12 @@ export default function ThrottlePage() {
     // any expensive operation or API call
   };
 
+  const throttledHandleResize = useThrottle(handleResize, 1000);
+
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
+    window.addEventListener("resize", throttledHandleResize);
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("resize", throttledHandleResize);
     };
   });
 
