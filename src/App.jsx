9f9d9c20fb9b028983f5b03ad418ca678f0ad7e2
AnishMandal939 <mandalktaanees12@gmail.com>
2024-03-12 02:08:00 +0545

    Replace Quiz component with ThrottlePage component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import Quiz from './pages/quiz-app/Quiz'
+import ThrottlePage from './pages/Throttle-page/ThrottlePage'
+// import Quiz from './pages/quiz-app/Quiz'
 
 export default function App (){
 
   return (
     <div>
-      <Quiz />
+      {/* <Quiz /> */}
+      <ThrottlePage />
     </div>
   )
 }
